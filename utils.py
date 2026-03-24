7c056ef7f12defb9cb9a11155c60ebc214da61fc
nilozdemir <nil.ozdemir@std.yildiz.edu.tr>
2026-03-24 20:52:04 +0300

    added custom label smoothing for kd

--- utils.py
+++ utils.py
@@ -17,9 +17,28 @@ class DistillationLoss(nn.Module):
         # Soft loss (distillation)
         student_log_probs = F.log_softmax(student_logits / self.T, dim=1)
         teacher_probs = F.softmax(teacher_logits / self.T, dim=1)
-
+        #labelsmoothing
+        nc = teacher_probs.size(1)
+        teacher_probs = label_sm(teacher_probs,targets,nc)
         loss_kl = self.kl(student_log_probs, teacher_probs)
 
         # Combine losses
         loss = self.alpha * loss_ce + (1 - self.alpha) * (self.T ** 2) * loss_kl
         return loss
+    
+@torch.no_grad()
+def label_sm(teach_prob,target,nc):
+        batch = teach_prob.size(0)
+        
+        true_idx = torch.argmax(teach_prob, dim=1)
+        true_prob = torch.max(teach_prob, dim=1).values
+        rest_prob = (1-true_prob) / (nc-1)
+
+        tensor_list = []
+        for b in range(batch):
+          singbatch = torch.full((1, nc), rest_prob[b].detach())
+          tensor_list.append(singbatch)
+        final = torch.stack(tensor_list, dim=0).squeeze()
+        #broadcasting
+        final[torch.arange(batch), true_idx] = true_prob
+        return final
